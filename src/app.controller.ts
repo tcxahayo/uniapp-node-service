7c8c41c027bb0dd8d0bb35f0cc35c46240079070
tcxahayo <1392091646@qq.com>
2024-03-20 16:24:03 +0800

    #FEAT: init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('getHello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('getName')
+  getName(@Body() params): string {
+    return `hello ${params.name}`;
   }
 }
